5e98dc33227f9489306efecac08e61db6daa77ef
rakkun9 <jnicolaybv@gmail.com>
2024-07-04 11:45:53 -0500

    First commit

---
+++ src/main/java/anime/talks/AnimetalksApplication.java
@@ -0,0 +1,13 @@
+package anime.talks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnimetalksApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AnimetalksApplication.class, args);
+    }
+
+}
---
+++ src/test/java/anime/talks/AnimetalksApplicationTests.java
@@ -0,0 +1,13 @@
+package anime.talks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnimetalksApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
